d25fb18ba17572c081ea0454e5dc46ce245e4ec5
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-02-15 15:28:13 +0100

    3rd example

---
+++ Strings/replace-text-with-string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$my_str = 'If the facts do not fit the theory, change the facts.';
+ 
+// Display replaced string
+echo str_replace("facts", "truth", $my_str);
+?>
+
+</body>
+</html>
