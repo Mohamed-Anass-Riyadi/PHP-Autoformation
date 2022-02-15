abc593425887e3e466bf00ca8202896b019eef28
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-02-15 15:22:57 +0100

    First example in strings

---
+++ Strings/length-in-string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$my_str = 'Welcome to Tutorial Republic';
+ 
+// Calculating and displaying string length
+echo strlen($my_str);
+?>
+
+</body>
+</html>
