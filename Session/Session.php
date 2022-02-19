d5272b236eda6c28fec1a7e4538f69a89d619e90
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-02-20 00:32:56 +0100

    Create Session.php

---
+++ Session/Session.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" >
+    <input type="text" name="username" placeholder="user">
+    <input type="password" name="pass" placeholder="password">
+    <input type="submit" name="btn" value="login">
+</form>
+
+    <?php
+    
+    $name = $_POST['username'] ?? '';
+    $pass = $_POST['password'] ?? '';
+
+    if(isset($_POST['btn'])){
+        if(empty($name) || ($pass)){
+            echo "<span style= color:red> CHAMP </span>";
+        }
+    }
+
+    ?>
+
+</body>
+</html>
