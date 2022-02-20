51ccb6be1bc95e1956db9c365dede10aeb038558
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-02-20 01:17:37 +0100

    up

---
+++ Files & INPUT OUTPUT/1example.php
@@ -0,0 +1,16 @@
+<html>
+
+   <head>
+      <title>Reading a file using PHP</title>
+   </head>
+   
+   <body>
+    <!-- r option -->
+      <?php
+         $file = fopen( 'text.txt', 'r' );
+         echo $file
+         
+      ?>
+      
+   </body>
+</html>
