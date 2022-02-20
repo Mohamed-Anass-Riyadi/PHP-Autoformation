b0ec8821ebbe7417ce2bd4089ab02ddcc1eee782
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-02-20 01:52:07 +0100

    Create Example.php

---
+++ Date&Time/Example.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo date('y m d l h i s a')
+    //  DATE : y = year / m = month / d = day / l = lowercase 
+    // TIME : h = hour / i = minutes / s = secondes / a = am or pm
+    ?>
+</body>
+</html>
